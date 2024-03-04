5e7284774cc0019a735b8d9f3ebe7a867cb90f1e
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-04 11:46:52 +0100

    add info za nas java klasa

---
+++ src/main/java/InformaciiZaNas.java
@@ -0,0 +1,13 @@
+
+public class InformaciiZaNas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ime="Marko";
+		int tezina=63;
+		char krvGrupa='A';
+		
+		System.out.println("Ime: "+ime+", tezina: "+tezina+", krvna grupa: "+krvGrupa);
+	}
+
+}
